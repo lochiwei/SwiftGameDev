85bd4ab01c695d100be52a86e4d30e87c92ea219
lochiwei <lochiwei.ios.dev.free@gmail.com>
2018-07-31 18:02:51 +0800

    add materials to node

--- SwiftGameDev/Scene+Extensions.swift
+++ SwiftGameDev/Scene+Extensions.swift
@@ -9,16 +9,45 @@
 import SceneKit
 import Extensions
 
+// SCNNode
+extension SCNNode {
+    
+    // earth node
+    func newEarthNode(_ radius:CGFloat, at position:SCNVector3? = nil) -> SCNNode {
+        let earthNode = newSphereNode(radius, at: position)
+        let m = earthNode.geometry!.firstMaterial!
+        m.diffuse.contents = #imageLiteral(resourceName: "earthDiffuse")
+        m.specular.contents = #imageLiteral(resourceName: "earthSpecular")
+        m.normal.contents = #imageLiteral(resourceName: "earthNormal")
+        m.ambient.contents = #imageLiteral(resourceName: "earthAmbient")
+        m.diffuse.mipFilter = .linear
+        return earthNode
+    }
+    
+    // box rock
+    func newBoxRock(_ width:CGFloat, _ height:CGFloat, _ length:CGFloat, at position:SCNVector3? = nil) -> SCNNode  {
+        
+        // prepare materials
+        let materials = (1...6).map { (i) -> SCNMaterial in
+            let m = SCNMaterial()
+            m.diffuse.contents = UIImage(named: "boxSide\(i)")
+            return m
+        }
+        
+        // new box node
+        let node = newBoxNode(width, height, length, at: position)
+        node.geometry?.materials = materials
+        
+        return node
+    }
+    
+}// end: SCNNode extension
+
+
 // scene
 extension SCNScene {
     
     func setup() {
-        
-        // nodes
-        _ = rootNode.newShape(
-            SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0),
-            at: [0, 0.5, 0], diffuse: UIColor.red
-        )
         
         // camera
         _ = rootNode.newCameraNode(at: [10, 15, 30], lookAt: rootNode)
@@ -26,11 +55,11 @@ extension SCNScene {
         // lights
         _ = rootNode.newLightNode(.omni, at: [0, 15, 15])
         _ = rootNode.newLightNode(.ambient, color: UIColor.darkGray)
+        spotlight()
         
-        // floor
+        // nodes
+        _ = rootNode.newBoxNode(1, 1, 1, at: [0, 0.5, 0], diffuse: UIColor.red)
         _ = rootNode.newFloorNode(diffuse: "Floor")
-        
-        // primitive nodes
         _ = rootNode.newPyramidNode(3, 6, 3, at: [30, 0, -40], diffuse: UIColor.blue)
         _ = rootNode.newSphereNode(6, at: [35, 6, -60], diffuse: UIColor.red)
         _ = rootNode.newBoxNode(3,3,3, at: [20, 1.5, -20], diffuse: UIColor.brown)
@@ -38,14 +67,25 @@ extension SCNScene {
         _ = rootNode.newCylinderNode(r:3, h:3, at: [0,4,-25], diffuse: UIColor.green)
         _ = rootNode.newTorusNode(R: 7, r: 2, at: [75, 1, -80], diffuse: UIColor.orange)
         
+        // nodes with materials
+        _ = rootNode.newEarthNode(10, at: [-20, 10, -30])
+        _ = rootNode.newBoxRock(10, 10, 10, at: [-20, 10, 0])
+        
     }// end: setup()
     
-}// end: SCNNode extension
-
-
-// my extension
-extension SCNNode {
-    
-    
+    func spotlight() {
+        
+        let node = rootNode.newLightNode(
+            .spot, color: UIColor.darkGray,
+            at: [10, 50, 30], lookAt: rootNode
+        )
+        
+        let spotlight = node.light!
+        spotlight.castsShadow = true
+        spotlight.spotInnerAngle = 0 // degrees
+        spotlight.spotOuterAngle = 50
+        spotlight.zNear = 50
+        spotlight.zFar = 500
+    }
     
 }// end: SCNNode extension
